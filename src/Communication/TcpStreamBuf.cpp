eebe4e7f9aa370d367ce30a86beebbbb3c673696
verxirtam <d-kosuga@jcom.home.ne.jp>
2015-07-19 23:55:16 +0900

    クラス図とシーケンス図を作成

---
+++ src/Communication/TcpStreamBuf.cpp
@@ -0,0 +1,24 @@
+/*
+ * TcpStreamBuf.cpp
+ *
+ *  Created on: 2015/07/19
+ *      Author: daisuke
+ */
+
+#include "TcpStreamBuf.h"
+
+namespace RL
+{
+
+TcpStreamBuf::TcpStreamBuf()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+TcpStreamBuf::~TcpStreamBuf()
+{
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace RL */
