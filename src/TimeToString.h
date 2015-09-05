d77bb63410188d35ff6e158d480c547d9236d6df
verxirtam <d-kosuga@jcom.home.ne.jp>
2015-09-06 03:22:27 +0900

    TimeToStringにファイル名用の関数を追加

--- src/TimeToString.h
+++ src/TimeToString.h
@@ -16,17 +16,32 @@ namespace RL
 
 class TimeToString
 {
+private:
 public:
+	//time_t型の時刻を文字列に変換する("YYYY/MM/DD HH:MM:SS")
 	static std::string toString(const std::time_t& time_)
 	{
-		const int size = 64;
+		std::string pattern("%Y/%m/%d %H:%M:%S");
+
+		return TimeToString::toStringPattern(time_,pattern);
+	}
+	//time_t型の時刻を文字列に変換する("YYYYMMDDHHMMSS")
+	static std::string toStringForFileName(const std::time_t& time_)
+	{
+		std::string pattern("%Y%m%d%H%M%S");
+
+		return TimeToString::toStringPattern(time_,pattern);
+	}
+	//time_t型の時刻をpatternに応じて文字列に変換する
+	static std::string toStringPattern(const std::time_t& time_, const std::string& pattern,const int size = 64)
+	{
 		char timestring[size];
 		std::tm* construct_time_tm = std::localtime(&time_);
-		char pattern[] = "%Y/%m/%d %H:%M:%S";
-		std::strftime(timestring,size,pattern,construct_time_tm);
+		std::strftime(timestring,size,pattern.c_str(),construct_time_tm);
 
 		return std::string(timestring);
 	}
+
 };
 
 } /* namespace RL */
