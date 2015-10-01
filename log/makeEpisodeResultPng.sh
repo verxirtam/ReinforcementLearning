b912e7ba07eedaee5dc1aaba9def414f80d43762
verxirtam <d-kosuga@jcom.home.ne.jp>
2015-10-02 01:22:20 +0900

    DiscountRateを1.0にした実機検証を実施 信頼区間が小さく見積もられすぎているように見える。 信頼区間自体がエピソードごとに大きく移動している。 信頼区間の算出方法を確認する必要あり。 コストはJava側でミリ秒単位で算出されていたのでコストは問題な無し

--- log/makeEpisodeResultPng.sh
+++ log/makeEpisodeResultPng.sh
@@ -3,6 +3,6 @@ gnuplot << EOF
 set terminal png size 1280,960
 set out "$1.png"
 set xrange [0:5]
-set yrange [0:2]
+set yrange [0:70]
 plot "$1.log" using (\$1+\$2*0.1):(\$9):(\$11):(\$12) with errorbars
 EOF
